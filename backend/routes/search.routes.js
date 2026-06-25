e72125abdc8c57229c43a7816a7602a3d1d38735
tusharsolanki9845-dev <tusharsolanki9845@gmail.com>
2026-06-25 09:22:30 +0530

    Clean search routes file

--- backend/routes/search.routes.js
+++ backend/routes/search.routes.js
@@ -9,15 +9,4 @@ const {
 router.get("/", searchBusinesses);
 router.post("/save", saveLead);
 
-const express = require("express");
-const router = express.Router();
-
-const {
-  searchBusinesses,
-  saveLead
-} = require("../controllers/search.controller");
-
-router.get("/", searchBusinesses);
-router.post("/save", saveLead);
-
 module.exports = router;
